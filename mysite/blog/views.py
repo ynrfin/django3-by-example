7dbe4710004859c2c0a4c314835c0e2f98777923
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2020-05-10 21:57:33 +0700

    add comment on detail page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 class PostListView(ListView):
@@ -37,10 +37,29 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create comment object but don't save to DB yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the commit
+            new_comment.post = post
+            # save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
     return render(request,
         'blog/post/detail.html',
-        {'post': post})
+        {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form
+    })
 
 def post_share(request, post_id):
     # REtrieve post by id
